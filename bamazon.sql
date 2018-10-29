5d6cbb08223c1a3c16c8d904325e39ef5b6458a8
mjbenefiel <michaeljbenefiel@gmail.com>
2018-10-29 17:27:36 -0500

    10292018 commits initial commits

---
+++ bamazon.sql
@@ -0,0 +1,21 @@
+-- Create a database called 'Bamazon' and switch into it for this activity --
+CREATE DATABASE bamazon;
+USE bamazon;
+
+-- Create a table called 'products' which will contain the store inventory --
+CREATE TABLE products (
+	product_id INTEGER (11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(90) NOT NULL,
+	department_name VARCHAR(90) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(10) NOT NULL,
+	PRIMARY KEY (product_id)
+);
+
+-- Insert data into the 'products' table --
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ('Echo Dot', 'Amazon Devices', 49.99, 5000),
+		('Fire TV Stick', 'Amazon Devices', 39.99, 3928),
+		('TubShroom', 'Amazon Launchpad', 12.99, 13000),
+		('Soylent Meal Replacement', 'Amazon Launchpad', 39.00, 503231);
+		
